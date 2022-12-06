e8b4e8bfb43e411128d312398ea338b19f2cce67
Ogoh Blessing <ogoh_blessing@yahoo.com>
2022-12-06 01:31:42 +0000

    Change format for fetching API data

--- scripts/index.js
+++ scripts/index.js
@@ -22,52 +22,59 @@ function showSlides() {
 
 
 
-let img = document.getElementById("test");/*
+let img = document.getElementById("img");/*
 let name = document.getElementById("");
 let price = document.getElementById("");
 */
 
-let carouselUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/439bc2dd8693b490539eae236918f4a53dd17457/carousel.json";
-let auctionUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/auction.json";
-let dropsUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/drops.json";
-let productsUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/products.json";
+  let carouselUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/439bc2dd8693b490539eae236918f4a53dd17457/carousel.json";
+  let auctionUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/auction.json";
+  let dropsUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/drops.json";
+  let productsUrl = "https://gist.githubusercontent.com/eniiku/65a95533de1f005eee35d5eb91f3e141/raw/c7188e070a8670a86883ff57224dcad277814761/products.json";
 
-window.onload = (event) => {
+  // Defining async function
+  async function getapi(productsUrl) {
 
-  fetch(productsUrl)
-      .then(res => res.json())
-      .then(result => {
-        console.log(result);
-        img.src = result.products[0].url;
-      })
-      .catch(err=>console.log(err))
+    // Storing response
+    const response = await fetch(productsUrl);
 
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    //if (response) {
+      //hideloader();
+    //}
+    show(data);
 
-  fetch(auctionUrl)
-      .then(res => res.json())
-      .then(result => {
-        console.log(result)
-        /* img.src = result.message */
-      })
-      .catch(err=>console.log(err))
+  }
+  // Calling that async function
+  getapi(productsUrl);
 
+  // Function to hide the loader
+  //function hideloader() {
+    //.getElementById('loading').style.display = 'none';
+  //}
+  // Function to define innerHTML for HTML table
+  function show(data) {
+    let card =
+      `<div>
+        <img id="img" src="" alt="">
+        <p id="name"></p>
+        <h3 id="price"></h3>
+      </div>`;
 
-      fetch(dropsUrl)
-      .then(res => res.json())
-      .then(result => {
-        console.log(result)
-        /* img.src = result.message */
-      })
-      .catch(err=>console.log(err))
-
-
-      fetch(carouselUrl)
-      .then(res => res.json())
-      .then(result => {
-        console.log(result)
-        /* img.src = result.message */
-      })
-      .catch(err=>console.log(err))
-};
+    // Loop to access all rows
+    for (let a of data.products) {
+      card += 
+      `<div class="col-md-4">
+        <img id="img" src= ${a.url} alt="">
+        <p id="name">${a.name}</p>
+        <h3 id="price">${a.price.usd}</h3>
+      </div>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("products").innerHTML = card;
+  }
+//};
 
 
